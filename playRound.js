7f90013bcb20f11c2e940cedd9ccaff480a1b706
tjadea <tjargent@outlook.com>
2018-02-23 16:31:10 +0000

    tjadea

--- playRound.js
+++ playRound.js
@@ -6,20 +6,20 @@ function playRound() {
         return "Computer chooses " + computerSelection + "!" + "\n" + "Its a tie!" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
     else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
         computerWin++;
-        return "Computer chooses Rock!" + "\n" + "Computer wins, Rock beats Scissors" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
+        return "Computer chooses ROCK!" + "\n" + "Computer wins, ROCK beats SCISSORS" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
     else if (computerSelection === 'ROCK' && playerSelection === 'PAPER') {
         playerWin++;
-        return "Computer chooses Rock!" + "\n" + "Player wins, Paper beats Rock" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }        
+        return "Computer chooses ROCK!" + "\n" + "Player wins, PAPER beats ROCK" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }        
     else if (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') {
         playerWin++;
-        return "Computer chooses Paper!" + "\n" + "Player wins, Scissors beat Paper" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
+        return "Computer chooses PAPER!" + "\n" + "Player wins, SCISSORS beat PAPER" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
     else if (computerSelection === 'PAPER' && playerSelection === 'ROCK') {
         computerWin++;
-        return "Computer chooses Paper!" + "\n" + "Computer wins, Paper beats Rock" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
+        return "Computer chooses PAPER!" + "\n" + "Computer wins, PAPER beats ROCK" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
     else if (computerSelection === 'SCISSORS' && playerSelection === 'ROCK') {
         playerWin++;
-        return "Computer chooses Scissors!" + "\n" + "Player wins, Rock beats Scissors" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
+        return "Computer chooses SCISSORS!" + "\n" + "Player wins, ROCK beats SCISSORS" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
     else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
         computerWin++;
-        return "Computer chooses Scissors!" + "\n" + "Computer wins, Scissors beat Paper" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
+        return "Computer chooses SCISSORS!" + "\n" + "Computer wins, SCISSORS beat PAPER" + "\n" + "Score (Comp): " + computerWin + "\n" + "Score (You): " + playerWin; }
 };
